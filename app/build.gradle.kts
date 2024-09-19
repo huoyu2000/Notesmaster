43e6b0281d3fc3834450ccd3d4fee7aaac8a7c7d
禍雨 <1428660523@qq.com>
2024-09-19 20:35:13 +0800

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "net.micode.notes"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "net.micode.notes"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    packaging {
+        resources.excludes.add("META-INF/DEPENDENCIES");
+        resources.excludes.add("META-INF/NOTICE");
+        resources.excludes.add("META-INF/LICENSE");
+        resources.excludes.add("META-INF/LICENSE.txt");
+        resources.excludes.add("META-INF/NOTICE.txt");
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(files("..\\httpcomponents-client-4.5.14-bin\\lib\\httpclient-osgi-4.5.14.jar"))
+    implementation(files("..\\httpcomponents-client-4.5.14-bin\\lib\\httpclient-win-4.5.14.jar"))
+    implementation(files("..\\httpcomponents-client-4.5.14-bin\\lib\\httpcore-4.4.16.jar"))
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
